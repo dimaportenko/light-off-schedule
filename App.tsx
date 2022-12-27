ccfeaaccd1ac2526438a9d1cb15eea4598baa091
Dima Portenko <troublesohard@gmail.com>
2022-12-27 23:13:34 +0200

    reload on foreground

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { ActivityIndicator, View } from "react-native";
+import { View } from "react-native";
 import tw from "./src/lib/tailwind";
 import { MainScreen } from "./src/screens/MainScreen";
 import {
@@ -9,24 +9,21 @@ import {
 } from "./src/hooks/useForeground";
 
 export default function App() {
-  const [loading, setLoading] = React.useState(false);
+  const [_, setLoading] = React.useState(false);
 
   useForegroundSubscription();
 
   useForegroundListener(() => {
     setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 100);
   });
 
-  if (loading) {
-    return (
-      <View style={tw`flex-1 bg-white justify-center items-center`}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <View style={tw`flex-1 bg-white justify-center items-center`}>
+  //       <ActivityIndicator size="large" />
+  //     </View>
+  //   );
+  // }
 
   return (
     <View style={tw`flex-1`}>
