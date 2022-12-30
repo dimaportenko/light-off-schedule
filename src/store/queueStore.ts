e74aa2be89f7726ea51319fc754d3d273e2a5b07
Dima Portenko <troublesohard@gmail.com>
2022-12-31 00:11:30 +0200

    persist mobx queue store

--- src/store/queueStore.ts
+++ src/store/queueStore.ts
@@ -1,12 +1,41 @@
-import { makeAutoObservable } from "mobx";
+import { autorun, makeAutoObservable, runInAction } from "mobx";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const createQueueStore = () => {
+  const storeKey = "@queue.store";
   const store = makeAutoObservable({
-    selectedQueueIndex: 0,
+    selectedQueueIndex: 2,
 
     setSelectedQueueIndex: (index: number) => {
       store.selectedQueueIndex = index;
     },
+  });
+
+  const hydrate = async () => {
+    const state = await AsyncStorage.getItem(storeKey);
+
+    if (state) {
+      const parsedState = JSON.parse(state);
+      runInAction(() => {
+        store.selectedQueueIndex = parsedState.selectedQueueIndex;
+      });
+    }
+  };
+
+  const persist = async () => {
+    const state = JSON.stringify({
+      selectedQueueIndex: store.selectedQueueIndex,
+    });
+
+    try {
+      await AsyncStorage.setItem(storeKey, state);
+    } catch (error) {
+      console.warn("Could not persist data", error);
+    }
+  };
+
+  hydrate().then(() => {
+    autorun(persist);
   });
 
   return store;
