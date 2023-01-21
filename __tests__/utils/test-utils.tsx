26ab634f587362a9f03305c48af4383dfd70338d
Dima Portenko <troublesohard@gmail.com>
2023-01-21 21:44:48 +0200

    test: freeze time for specific date for tests

--- __tests__/utils/test-utils.tsx
+++ __tests__/utils/test-utils.tsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react-native";
 import { root, StoreProvider } from "../../src/store";
 
+// ---- Date config Start ----
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
@@ -8,6 +9,13 @@ dayjs.extend(relativeTime);
 // add custom parse format plugin
 import customParseFormat from "dayjs/plugin/customParseFormat";
 dayjs.extend(customParseFormat);
+
+import tk from "timekeeper";
+const startTimestamp = 1487076708000;
+const time = new Date(startTimestamp);
+tk.freeze(time);
+
+// ---- Date config End ----
 
 const AllTheProviders = ({ children }: { children: any }) => {
   return <StoreProvider value={root}>{children}</StoreProvider>;
