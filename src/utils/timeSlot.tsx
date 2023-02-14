ab9cdab1fbdc0fd374e7e915abd6a0b0702dbc7d
Dima Portenko <troublesohard@gmail.com>
2023-02-14 22:26:56 +0200

    wip: add svg icons dependencies, replace main screen emojies

--- src/utils/timeSlot.tsx
+++ src/utils/timeSlot.tsx
@@ -1,4 +1,8 @@
 import { TimeSlotType } from "../data/schedule";
+import OnIcon from "../../assets/svg/ic_on.svg";
+import OffIcon from "../../assets/svg/ic_off.svg";
+import MaybeIcon from "../../assets/svg/ic_maybe.svg";
+import { SvgProps } from "react-native-svg";
 
 // export const getSlotTypeEmoji = (type: TimeSlotType) => {
 //   switch (type) {
@@ -23,5 +27,18 @@ export const getSlotTypeEmoji = (type: TimeSlotType) => {
       return "🌗";
     default:
       return "🌕";
+  }
+};
+
+export const getSlotTypeIcon = (type: TimeSlotType, props: SvgProps) => {
+  switch (type) {
+    case "on":
+      return <OnIcon {...props} />;
+    case "off":
+      return <OffIcon {...props} />;
+    case "maybe":
+      return <MaybeIcon {...props} />;
+    default:
+      return <OnIcon {...props} />;
   }
 };
